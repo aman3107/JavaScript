e1b8899d8b70565199ecc50f5c89cfdce12d10e7
Aman Mittal <amittal@mirrahealthcare.com>
2023-10-02 21:51:59 +0530

    Day1

---
+++ Intro_Course/script.js
@@ -0,0 +1,4 @@
+console.log("Hello World");
+console.log("Hello Aman");
+console.log("hello Payal");
+console.log("Hello")
---
+++ Variables_dataTypes/script.js
@@ -0,0 +1,31 @@
+console.log("Welcome To Variables")
+
+var variableName = "Welcome to Variables";
+console.log(variableName);
+
+let variableNamelet = "Welcome Aman";
+const variableNameconst = "Welocome Payal";
+
+console.log(variableNamelet);
+console.log(variableNameconst);
+
+// We getting error if we change the value of a const varible
+// variableNameconst = 'Aman';
+
+
+/* Data Types */
+
+//String
+// const exampleString = 'Hello, World';
+// console.log(exampleString)
+
+// const singlQuotes = 'Hello!';
+// console.log(singlQuotes);
+
+// const doubleQuotes = "Hello!!";
+// console.log(doubleQuotes);
+
+const name = 'Aman';
+
+const backticks = `Hello!!! ${name}`;
+console.log(backticks);
