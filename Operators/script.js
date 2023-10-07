c67ad8f8fe77fe96c2860899b91ff691733bc174
Aman Mittal <amittal@mirrahealthcare.com>
2023-10-07 15:43:19 +0530

    Operators Intro

---
+++ Operators/script.js
@@ -0,0 +1,62 @@
+console.log("Introduction of Operators");
+
+// const a = 5;
+// const b = 10;
+
+// let result = 0;
+
+// Addition
+// result = a + b;
+// console.log(result);
+
+//Subtraction
+// result = a - b;
+// console.log(result);
+
+// Multiplication
+// result = a * b;
+// console.log(result);
+
+// Division
+// result = a / b;
+// console.log(result);
+
+// Exponent
+
+// result = a ** b;
+// console.log(result)
+
+// Modulo
+
+// result = a % b;
+// console.log(result);
+
+// Increment and Decrement
+
+// result++;
+// console.log(result);
+// result--;
+// console.log(result);
+
+// Comparison and Equality Operators
+
+const a = 5;
+const b = 10;
+
+// Greater Than
+console.log(a > b);
+
+// Greater than or equal to
+console.log(a >= b);
+
+// Less Than
+console.log(a < b);
+
+// Less than or equal to
+console.log(a <= b);
+
+// Is Equal
+console.log(a == b);
+
+// Not Equal
+console.log(a != b);
--- Variables_dataTypes/script.js
+++ Variables_dataTypes/script.js
@@ -1,13 +1,13 @@
-console.log("Welcome To Variables")
+// console.log("Welcome To Variables")
 
-var variableName = "Welcome to Variables";
-console.log(variableName);
+// var variableName = "Welcome to Variables";
+// console.log(variableName);
 
-let variableNamelet = "Welcome Aman";
-const variableNameconst = "Welocome Payal";
+// let variableNamelet = "Welcome Aman";
+// const variableNameconst = "Welocome Payal";
 
-console.log(variableNamelet);
-console.log(variableNameconst);
+// console.log(variableNamelet);
+// console.log(variableNameconst);
 
 // We getting error if we change the value of a const varible
 // variableNameconst = 'Aman';
@@ -25,7 +25,63 @@ console.log(variableNameconst);
 // const doubleQuotes = "Hello!!";
 // console.log(doubleQuotes);
 
-const name = 'Aman';
+// const name = 'Aman';
 
-const backticks = `Hello!!! ${name}`;
-console.log(backticks);
+// const backticks = `Hello!!! ${name}`;
+// console.log(backticks);
+
+// Number
+
+// const firstNumber = 5;
+// const strin = 'hello';
+// const secondNumber = 10;
+
+// const result = strin / secondNumber;
+
+// console.log(typeof result);
+
+// Booleans
+
+// const isCool = false;
+// console.log(isCool);
+
+// console.log(typeof isCool);
+
+// if(isCool){
+//   console.log("Hi man, you're cool");
+// }
+// else{
+//   console.log('Hey');
+// }
+
+
+// Null
+
+// let age = null;
+// console.log(typeof age);
+// console.log(age);
+// age = 20;
+// console.log(typeof age);
+// console.log(age);
+
+// Undefined
+
+// let x;
+// console.log(x);
+// console.log(typeof x);
+
+// Objects
+
+// const name = 'john';
+// const age = 20;
+
+// const person = {
+//   name: 'john',
+//   age: 20
+// };
+
+// console.log(person['age']);
+
+// const date = new Date();
+// console.log(date);
+
