dbcc2cce0b52ba18bf1927d802b59139157fe564
Aman Mittal <amittal@mirrahealthcare.com>
2023-10-09 13:53:29 +0530

    Operators

--- Operators/script.js
+++ Operators/script.js
@@ -40,23 +40,78 @@ console.log("Introduction of Operators");
 
 // Comparison and Equality Operators
 
-const a = 5;
-const b = 10;
+// const a = 5;
+// const b = 10;
 
-// Greater Than
-console.log(a > b);
+// // Greater Than
+// console.log(a > b);
 
-// Greater than or equal to
-console.log(a >= b);
+// // Greater than or equal to
+// console.log(a >= b);
 
-// Less Than
-console.log(a < b);
+// // Less Than
+// console.log(a < b);
 
-// Less than or equal to
-console.log(a <= b);
+// // Less than or equal to
+// console.log(a <= b);
 
-// Is Equal
-console.log(a == b);
+// // Is Equal
+// console.log(a == b);
 
-// Not Equal
-console.log(a != b);
+// // Not Equal
+// console.log(a != b);
+
+
+// Strict Equality
+// console.log(a === b);
+
+// // Strict Inequality
+// console.log(a !== b);
+
+// Strict Equality
+// Compare Values and Data Types
+// return true only both are the same
+
+// console.log(5 === "5");
+
+// Loose Equality
+// Compares only values not data types
+
+// console.log(5 == "5");
+
+// The Good Ones : ===, !==
+// The Evil Ones : ==, !=
+
+// console.log('' == '0');
+// console.log(0 == '');
+// console.log('0' == 0)
+
+// console.log(false == 'false');
+// console.log('0' == false);
+
+// console.log(2  == true)
+
+// Logical Operators
+// AND (&&) => ALL OPERANDS ARE TRUE => TRUE
+// console.log(true && true);
+// console.log(true && true && false);
+// OR (||) => AT LEAST ONE OPERAND IS TRUE => TRUE
+// console.log(true || false);
+// NOT (!) => REVERSE THE BOOLEAN VALUE
+// console.log(!true);
+
+// ASSIGNMENT OPERATORS
+
+// let number = 5;
+// number += 5; // number = number + 5;
+// console.log(number);
+// number -= 5; // number = number - 5;
+// console.log(number);
+// number *= 5; // number = number * 5;
+// console.log(number);
+// number /= 5; // number = number / 5;
+// console.log(number);
+
+
+
+
