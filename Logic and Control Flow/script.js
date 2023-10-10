f1d050465388e40fcd0b7a1042c5fadf0a82a4d3
Aman Mittal <amittal@mirrahealthcare.com>
2023-10-10 12:55:02 +0530

    Control Flow

---
+++ Logic and Control Flow/script.js
@@ -0,0 +1,121 @@
+console.log("Introuction to Control Flow");
+
+// If Statement
+// const age = 24;
+
+// if(age > 18){
+//   console.log('You may enter')
+// }
+// else if(age === 18){
+//   console.log("You just turned 18,Welcome");
+// }
+// else{
+//   console.log("Grow Up !");
+// }
+
+// Truthy and Falsy Values
+
+// if([]){
+//   console.log("IN HERE");
+// }
+// else{
+//   console.log("NO, IN HERE!");
+// }
+
+// // Falsy Values
+// false
+// 0
+// ''
+// null
+// undefined
+// NaN
+
+// const age = 19;
+// const isCool = true;
+
+// if(isCool && age > 18){
+//   console.log("You May Enter");
+// }
+// else{
+//   console.log("You cannot enter");
+// }
+
+// console.log(age && age > 18);
+// console.log('truthy' && 0 && 'test' && 999);
+// console.log('' || 0 || null || undefined);
+
+// console.log(!true);
+// console.log(!false);
+// console.log(!!'');
+
+// const value = null;
+
+// if(value){
+//   console.log("value is truthy");
+// }
+// else{
+//   console.log("value is falsy");
+// }
+
+// Switch Statement
+
+// const superHero = 'BatMan';
+
+// switch(superHero){
+  
+//   case 'Iron Man':
+//     console.log("I am Iron Man...");
+//     break;
+//   case 'Captain America':
+//     console.log("I am Captain America... ");
+//     break;
+//   case 'Thor':
+//     console.log("I am Thor... ");
+//     break;
+//   case 'Black Widow':
+//     console.log("I am Black Widow");
+//     break;
+//   default:
+//     console.log("Enter a valid superHero Name");
+//     break;
+// }
+
+
+// TERNARY OPERATOR
+
+// const age = 18;
+
+// if(age > 18){
+//   console.log("You can drive!");
+// }
+// else{
+//   console.log("You cannot drive!");
+// }
+
+// age >= 18 ?  console.log("You can drive!"): console.log("You cannot drive!");
+
+
+// While Loop
+
+// while(condition){
+//   // code
+// }
+
+// let i = 0;
+// while(i < 10){
+//   console.log(i);
+//   i++;
+// }
+
+// For Loop
+
+// for([initialization]; [condition]; [updation]){
+//   // code
+// }
+
+for(let i = 0; i < 10; i++){
+  console.log(i)
+}
+
+
+
