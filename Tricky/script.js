b5fe4cf93614a7866b2e6063a8d7e2488e376756
Aman Mittal <amanmittalma@gmail.com>
2023-10-12 00:06:04 +0530

    Tricks Part

--- Tricky/script.js
+++ Tricky/script.js
@@ -1,1 +1,78 @@
 console.log("Introduction to Tricks");
+console.log("Hello My Name is Aman, Hi");
+
+// Global Scope
+
+// let name = "John";
+
+// const logName = () => {
+//   // name = "Aman";
+//   console.log(name);
+// };
+
+// const logName2 = () => {
+//   console.log(name);
+// };
+// logName();
+// logName2();
+
+// Local Scope/ Function Scope
+
+// const someFunction = () => {
+//   const name1 = "Aman";
+//   console.log(name1);
+
+//   const anotherFunction = () => {
+//     console.log( );
+//   };
+//   anotherFunction();
+// };
+// // console.log(name1);
+// someFunction();
+
+// Hoisting
+
+// age = 20;
+// console.log(age);
+// var age;
+
+// hoist();
+
+// function hoist() {
+//   message = "test";
+//   console.log(message);
+//   var message = "test";
+// }
+// hoist();
+
+// Closures
+
+// const outer = () => {
+//   const outerVar = "Hello!";
+//   const inner = () => {
+//     const innerVar = "Hi!";
+//     console.log(outerVar, innerVar);
+//   };
+//   console.log(outerVar);
+//   return inner;
+// };
+
+// // inner();
+// // outer();
+// const innerFn = outer();
+// innerFn();
+
+const init = () => {
+  const hobby = "Learning JavaScript";
+
+  const displayHobby = () => {
+    console.log(hobby);
+  };
+  return displayHobby;
+  // displayHobby();
+};
+
+// init();
+
+const dHobby = init();
+dHobby();
