0c5f3f0cbdc8e859280683d5c0b10e395967f07a
Aman Mittal <amittal@mirrahealthcare.com>
2023-10-11 15:04:39 +0530

    Functions

---
+++ Functions/script.js
@@ -0,0 +1,71 @@
+console.log("Introduction to Functions");
+
+// A BLOCK OF CODE => PERFORMS A TASK
+
+// A FUNCTION DECLARATION (defining a function)
+
+// function square(number) {
+//   console.log("I'm HERE");
+//   return number * number;
+// }
+
+// // A FUNCTION CALL (calling / executing a function)
+
+// console.log(square(10));
+
+// FUNCTION DECLARATION
+
+// function name(params) {
+//   // statements
+//   // have access to "this" keyword
+// }
+
+// // a function expression
+
+// const name1 = function (params) {
+//   // statements
+// };
+
+// // a arrow function
+// const name2 = (params) => {
+//   // statements
+// };
+
+// function sayHi(name) {
+//   console.log(`Hi, ${name}!`);
+// }
+
+// sayHi("Aman");
+
+// function add(a, b) {
+//   return "RETURN";
+//   return a + b;
+// }
+
+// const sum = add(2, 2);
+// console.log(sum);
+
+// function test() {
+//   return true;
+//   return false;
+// }
+
+// const bool = test();
+// console.log(bool);
+
+// ARROW FUNCTION
+
+// const square = (number) => {
+//   return number * number;
+// };
+
+// const mul = (a, b) => a * b;
+
+// const result = square(4);
+// console.log(result);
+
+// const mul_result = mul(2, 3);
+// console.log(mul_result);
+
+// Parameters: used when defining a function
+// Arguments: passed when making a function call
