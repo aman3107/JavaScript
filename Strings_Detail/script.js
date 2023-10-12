63baf0a21ac1ae768e419c0a4a9b6544a3c2d7b4
Aman Mittal <amittal@mirrahealthcare.com>
2023-10-12 15:53:09 +0530

    Strings

---
+++ Strings_Detail/script.js
@@ -0,0 +1,108 @@
+console.log("Introduction to Strings");
+
+// const single = "This is a string";
+// const double = "This is a string";
+// const backtick = `This is a string ${2 + 2}`;
+
+// console.log(single);
+// console.log(double);
+// console.log(backtick);
+
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+// const backtick2 = `This is a string ${sum(2, 2)}`;
+// console.log(backtick2);
+
+// const greeting = 'Hi, I\'m Aman, but people call me "Mittal"';
+// console.log(greeting);
+
+// String Length and Basic Properties
+
+// const name = "Aman";
+// console.log(name.length);
+// console.log(name[0]);
+
+// const firstLetter = name[0];
+// const lastLetter = name[name.length - 1];
+// console.log(firstLetter, lastLetter);
+
+// Change String Case
+
+// const mixedCaseString = "Hello! How are you?";
+// const lowerCaseString = mixedCaseString.toLowerCase();
+// const upperCaseString = mixedCaseString.toUpperCase();
+// console.log(lowerCaseString);
+// console.log(upperCaseString);
+
+// Searching for a SubString
+
+// const hobbies = "I Love HTML, CSS and JAVASCRIPT, JAVASCRIPT";
+// //indexOf()
+// const firstIndex = hobbies.indexOf("JAVASCRIPT", 20);
+// const index2 = hobbies.indexOf("React", 20);
+// console.log(firstIndex);
+
+// //lastIndexOf()
+// const lastIndex = hobbies.lastIndexOf("JAVASCRIPT");
+// console.log(lastIndex);
+
+// // includes
+
+// const includesJavaScript = hobbies.includes("JAVASCRIPT");
+// console.log(includesJavaScript);
+
+// // startsWith()
+
+// const startsWith = hobbies.startsWith("I Love");
+// console.log(startsWith);
+
+// // endsWith()
+
+// const endsWith = hobbies.endsWith("JAVASCRIPT");
+// console.log(endsWith);
+
+// Getting a SubString
+
+// const exampleString = "hotdog";
+
+// // slice()
+
+// const dog = exampleString.slice(3, 6);
+// console.log(dog);
+
+// Reverse, Repeat and Trim a String
+
+// const exampleString = "test";
+// const reverseString = exampleString.split("").reverse().join("");
+// console.log(reverseString);
+
+// // repeat()
+
+// const dogSays = "Woof";
+// console.log(dogSays.repeat(5));
+
+// // trim()
+
+// const exString = "Hello , World!  ";
+// console.log(exString.trim());
+
+// String Exercise
+
+// const guestList = "Our guests are: emma, jacob, isabella, ethan";
+
+// const lenString = guestList.length;
+// console.log(lenString);
+
+// const upperGuestList = guestList.toUpperCase();
+// console.log(upperGuestList);
+
+// const isEthanOnTheList = upperGuestList.includes("ETHAN");
+// console.log(isEthanOnTheList);
+
+// const subStringGuests = upperGuestList.slice(16);
+// console.log(subStringGuests);
+
+// const guests = subStringGuests.split(",");
+// console.log(guests);
